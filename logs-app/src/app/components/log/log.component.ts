8245ce77d353f27bab28bc4086b55a7499f4fa3c
Yotam Ronen <yotronen@gmail.com>
2020-07-22 16:50:42 +0300

    log component

--- logs-app/src/app/components/log/log.component.ts
+++ logs-app/src/app/components/log/log.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-log',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log.component.scss']
 })
 export class LogComponent implements OnInit {
+  @Input() content: string[] = [];
 
   constructor() { }
 
